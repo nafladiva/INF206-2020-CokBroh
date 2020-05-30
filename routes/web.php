e1a13e5c105f86df9257cf93a6027b853db34a68
cutmeutiahau <cut_meutia@mhs.unsyiah.ac.id>
2020-05-30 23:10:53 +0700

    Added route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/home', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile', function() {
+    return view('profile');
+});
