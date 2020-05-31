0742984bf1ce488c049c6a173497a54fb7061773
Desintha Zahara Azmi <desintha@mhs.unsyiah.ac.id>
2020-05-31 22:22:17 +0700

    Added frontend user profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="m-4 text-center">
+            <img src="https://f0.pngfuel.com/png/178/595/black-profile-icon-illustration-user-profile-computer-icons-login-user-avatars-png-clip-art.png" class="rounded-circle" alt="foto" style="max-width: 200px;">
+        </div>
+        <div class="text-center">
+            <h1 style="font-weight: bold;">Desintha Zahara Azmi</h1>
+        </div>
+        <div class="container mt-3 row justify-content-center">
+            <div class="col-8 row justify-content-center text-center">
+                <div class="col-3 m-2 p-2 col-profile">
+                <h1>24</h1>
+                <h3>Poin</h3>
+            </div>
+            <div class="col-3 m-2 p-2 col-profile">
+                <h1>24</h1>
+                <h3>Poin</h3></div>
+            <div class="col-3 m-2 p-2 col-profile">
+                <h1>24</h1>
+                <h3>Poin</h3></div>
+            </div>
+        </div>
+</div>
+@endsection
