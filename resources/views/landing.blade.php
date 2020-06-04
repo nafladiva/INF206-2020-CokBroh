eafea27b9e5d91e84c3504c41ee5f0e8411e219f
nafladiva <nafladiva@gmail.com>
2020-06-04 16:30:59 +0700

    fix responsive frontend

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.first')
 
-@section('title', 'Beranda - Whisper')
+@section('title', 'Beranda - CokBroh')
 
 @section('content')
   <div class="container landing">
-   <img src="{{asset('img/logo.png')}}" alt="logo" class="img-fluid logo">
+    <img src="{{asset('img/logo.png')}}" alt="logo" class="img-fluid logo">
   <p class="lead mb-0 mt-2">SELAMAT DATANG DI</p>
   <p class="nama">CokBroh</p>
   <hr class="my-4">
   <p>Kumpulkan sampahnya, dapatkan hadiahnya !!!</p>
   <p class="lead">
-    <a class="btn btn-warning btn-lg" href="{{ route('login') }}" role="button"><i class="fas fa-user-cog mr-2"></i>ADMIN</a>
-    <a class="btn btn-warning btn-lg" href="{{ url('/pengunjung') }}" role="button"><i class="fas fa-trash mr-2"></i>PENGUNJUNG</a>
+    <a class="btn btn-warning btn-lg m-2" href="{{ route('login') }}" role="button">
+      <i class="fa fa-unlock mr-2"></i>
+      ADMIN
+    </a>
+    <a class="btn btn-warning btn-lg m-2" href="{{ url('/pengunjung') }}" role="button"><i class="fa fa-trash mr-2"></i>PENGUNJUNG</a>
   </p>
 </div>
 @endsection
